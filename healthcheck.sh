993f04316f91123c97b292b77317edcf7deb4f15
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2020-09-21 11:32:10 +0200

    Fix the APIKEY env variable.

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
-APIKEY=$(grep ApiKey /config/config.xml | sed -r 's/<\/?ApiKey>//g')
 curl ${RADARR_URL}/api/v3/health?apiKey=${APIKEY} 2>&1 | grep -q error
 if [ "$?" -eq 1 ]
 then
-	# no errors
+  echo "No errors."
 	exit 0
 else
+  echo "Errors occured. Check the web interface."
 	exit 1
 fi
